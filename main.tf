56e0f1a29f86d2f166442427bc9caf5ef3deb3b9
Chris Gascoigne <cgascoig@cisco.com>
2023-11-28 09:43:33 +1100

    add anp

--- main.tf
+++ main.tf
@@ -14,6 +14,21 @@ resource "mso_schema_template" "demo_template" {
   template_type = "cloud_local"
 }
 
+resource "mso_schema_template_anp" "anp1" {
+  schema_id    = data.mso_schema.prod.id
+  template     = mso_schema_template.demo_template.name
+  name         = "${var.stack_name}-anp"
+  display_name = "${var.stack_name} application profile"
+}
+
+resource "mso_schema_template_anp_epg" "anp_epg" {
+  schema_id     = data.mso_schema.prod.id
+  template_name = mso_schema_template.demo_template.name
+  anp_name      = mso_schema_template_anp.anp1.name
+  name          = "front-end-svc"
+  display_name  = "front-end EPG"
+}
+
 
 # data "aws_ami" "ubuntu" {
 #   most_recent = true
