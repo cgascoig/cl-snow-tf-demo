f4379e05f2cd7c5ff2707e11f88b698acb46a0d8
Chris Gascoigne <cgascoig@cisco.com>
2023-11-28 12:42:03 +1100

    add contracts

--- main.tf
+++ main.tf
@@ -21,12 +21,129 @@ resource "mso_schema_template_anp" "anp1" {
   display_name = "${var.stack_name} application profile"
 }
 
-resource "mso_schema_template_anp_epg" "anp_epg" {
+resource "mso_schema_template_anp_epg" "front_end_svc" {
   schema_id     = data.mso_schema.prod.id
   template_name = mso_schema_template.demo_template.name
   anp_name      = mso_schema_template_anp.anp1.name
   name          = "front-end-svc"
-  display_name  = "front-end EPG"
+  display_name  = "front-end microservice EPG"
+}
+
+resource "mso_schema_template_anp_epg" "accounts_svc" {
+  schema_id     = data.mso_schema.prod.id
+  template_name = mso_schema_template.demo_template.name
+  anp_name      = mso_schema_template_anp.anp1.name
+  name          = "accounts-svc"
+  display_name  = "accounts microservice EPG"
+}
+
+resource "mso_schema_template_anp_epg" "catalogue_svc" {
+  schema_id     = data.mso_schema.prod.id
+  template_name = mso_schema_template.demo_template.name
+  anp_name      = mso_schema_template_anp.anp1.name
+  name          = "catalogue-svc"
+  display_name  = "catalogue microservice EPG"
+}
+
+resource "mso_schema_template_anp_epg" "notifications_svc" {
+  schema_id     = data.mso_schema.prod.id
+  template_name = mso_schema_template.demo_template.name
+  anp_name      = mso_schema_template_anp.anp1.name
+  name          = "notifications-svc"
+  display_name  = "notifications microservice EPG"
+}
+
+resource "mso_schema_template_contract" "fe_to_accounts_ctrct" {
+  schema_id     = data.mso_schema.prod.id
+  template_name = mso_schema_template.demo_template.name
+  contract_name = "front-end-to-accounts-ctrct"
+  display_name  = "front-end-to-accounts-ctrct"
+  filter_type   = "bothWay"
+  scope         = "context"
+  filter_relationship {
+    filter_name = "allow-https"
+  }
+  directives = ["none"]
+}
+
+resource "mso_schema_template_contract" "fe_to_catalogue_ctrct" {
+  schema_id     = data.mso_schema.prod.id
+  template_name = mso_schema_template.demo_template.name
+  contract_name = "front-end-to-catalogue-ctrct"
+  display_name  = "front-end-to-catalogue-ctrct"
+  filter_type   = "bothWay"
+  scope         = "context"
+  filter_relationship {
+    filter_name = "allow-https"
+  }
+  directives = ["none"]
+}
+
+resource "mso_schema_template_contract" "fe_to_notifications_ctrct" {
+  schema_id     = data.mso_schema.prod.id
+  template_name = mso_schema_template.demo_template.name
+  contract_name = "front-end-to-notifications-ctrct"
+  display_name  = "front-end-to-notifications-ctrct"
+  filter_type   = "bothWay"
+  scope         = "context"
+  filter_relationship {
+    filter_name = "allow-https"
+  }
+  directives = ["none"]
+}
+
+resource "mso_schema_template_anp_epg_contract" "fe_to_accounts_ctrct-prov" {
+  schema_id         = data.mso_schema.prod.id
+  template_name     = mso_schema_template.demo_template.name
+  anp_name          = mso_schema_template_anp.anp1.name
+  epg_name          = mso_schema_template_anp_epg.accounts_svc.name
+  contract_name     = mso_schema_template_contract.fe_to_accounts_ctrct.contract_name
+  relationship_type = "provider"
+}
+
+resource "mso_schema_template_anp_epg_contract" "fe_to_accounts_ctrct-cons" {
+  schema_id         = data.mso_schema.prod.id
+  template_name     = mso_schema_template.demo_template.name
+  anp_name          = mso_schema_template_anp.anp1.name
+  epg_name          = mso_schema_template_anp_epg.front_end_svc.name
+  contract_name     = mso_schema_template_contract.fe_to_accounts_ctrct.contract_name
+  relationship_type = "consumer"
+}
+
+resource "mso_schema_template_anp_epg_contract" "fe_to_catalogue_ctrct-prov" {
+  schema_id         = data.mso_schema.prod.id
+  template_name     = mso_schema_template.demo_template.name
+  anp_name          = mso_schema_template_anp.anp1.name
+  epg_name          = mso_schema_template_anp_epg.catalogue_svc.name
+  contract_name     = mso_schema_template_contract.fe_to_catalogue_ctrct.contract_name
+  relationship_type = "provider"
+}
+
+resource "mso_schema_template_anp_epg_contract" "fe_to_catalogue_ctrct-cons" {
+  schema_id         = data.mso_schema.prod.id
+  template_name     = mso_schema_template.demo_template.name
+  anp_name          = mso_schema_template_anp.anp1.name
+  epg_name          = mso_schema_template_anp_epg.front_end_svc.name
+  contract_name     = mso_schema_template_contract.fe_to_catalogue_ctrct.contract_name
+  relationship_type = "consumer"
+}
+
+resource "mso_schema_template_anp_epg_contract" "fe_to_notifications_ctrct-prov" {
+  schema_id         = data.mso_schema.prod.id
+  template_name     = mso_schema_template.demo_template.name
+  anp_name          = mso_schema_template_anp.anp1.name
+  epg_name          = mso_schema_template_anp_epg.notifications_svc.name
+  contract_name     = mso_schema_template_contract.fe_to_catalogue_ctrct.contract_name
+  relationship_type = "provider"
+}
+
+resource "mso_schema_template_anp_epg_contract" "fe_to_notifications_ctrct-cons" {
+  schema_id         = data.mso_schema.prod.id
+  template_name     = mso_schema_template.demo_template.name
+  anp_name          = mso_schema_template_anp.anp1.name
+  epg_name          = mso_schema_template_anp_epg.front_end_svc.name
+  contract_name     = mso_schema_template_contract.fe_to_catalogue_ctrct.contract_name
+  relationship_type = "consumer"
 }
 
 
